0dded113eebe2b8b0485ef219a90816edc779d3b
ythai <yvanthai@gmail.com>
2023-06-25 17:36:03 +0200

    ajouts casse des Stalactite

--- src/main/java/project/Stalactite.java
+++ src/main/java/project/Stalactite.java
@@ -1,13 +1,23 @@
 package project;
 
+import java.util.Random;
+
 public class Stalactite extends Structure {
+
+    private Random random = new Random();
 
     public Stalactite(double startX, double startY, double height, double thickness, CaveSimulation simulation) {
         super(startX, startY, height, thickness, simulation);
     }
 
+
     public void increaseHeight() {
-        height++;
+        double randomFactor = random.nextDouble(); // Génère un nombre aléatoire entre 0 et 1
+        if (randomFactor < 0.01) {
+            height = height/2; // Divise la hauteur avec une probabilité de 1%
+        } else {
+            height = height+1.5; // Augmente la hauteur avec une probabilité de 99%
+        }
         shape.setHeight(height);
     }
 
